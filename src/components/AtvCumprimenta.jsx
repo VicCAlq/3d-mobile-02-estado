f5516fbe88aa652c13b0f85e822d3d39b0a443c2
Bruna561 <bruna.nobrega@nave.org.br>
2026-06-15 16:07:21 -0300

    Update AtvCumprimenta.jsx

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,47 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+    
+
+import React, { useState } from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native';
+
+export default function AtvCumprimenta() {
+  const [nome, setNome] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  return (
+    <View>
+      <Text>Olá, qual o seu nome?</Text>
+
+      <View style={{ flexDirection: 'row' }}>
+        <TextInput
+          placeholder="Nome aqui"
+          value={nome}
+          onChangeText={setNome}
+          style={{
+            borderWidth: 1,
+            width: 150,
+            marginRight: 10
+          }}
+        />
+
+        <Pressable
+          onPress={() => {
+            setMensagem('É um prazer lhe conhecer, ' + nome);
+          }}
+          style={{
+            borderWidth: 1,
+            padding: 10
+          }}
+        >
+          <Text>Salvar</Text>
+        </Pressable>
+      </View>
+
+      <Text style={{ marginTop: 10 }}>
+        {mensagem}
+      </Text>
+    </View>
+  );
+}
