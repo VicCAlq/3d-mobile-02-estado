eb3717b14c18b3edb6e0a464ebfc3da0e6dc9555
tulinhokkj <anderson.macedo@nave.org.br>
2026-06-15 16:35:05 -0300

    atv dobrar

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,22 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import { useState } from 'react'
+
+export default function AtvDobra(numero) {
+  const [nmr, setNmr] = useState(1)
+  const dobrar = () => {
+    setNmr(nmr * 2)
+  }
+
+  return(
+    <View>
+      <Text>{nmr}</Text>
+      <Pressable onPress={dobrar}>
+        <Text>Dobrar</Text>
+      </Pressable>
+    </View>
+  )
+}
