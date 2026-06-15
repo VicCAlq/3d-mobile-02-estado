8f536588b72a606a3d646a1a601379261a323475
Seth <hattus.lima@nave.org.br>
2026-06-15 16:35:57 -0300

    aiaaiaiaiaia

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,65 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+
+
+import React, { useState } from "react";
+import { View,  StyleSheet, Text, TextInput, Pressable } from "react-native-web";
+
+const estilo = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderRadius: "5px",
+    backgroundColor: "#bbb",
+    padding: "4px",
+  }
+})
+
+export default function AtvCumprimenta(){
+ 
+ const [nome, setNome] = useState('');
+ const [mensagem, setMensagem] = useState('');
+    return(
+        <View>
+            <Text>
+                Olá, qual seu nome?
+            </Text>
+            <TextInput>
+
+            </TextInput>
+            <Pressable>
+
+            </Pressable>
+        </View>
+    )
+}
--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,58 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+
+const estilo = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+})
+
+export default function AtvDobra({numero}){
+
+    const [valor, setValor] = useState(numero)
+
+    function Dobrar(){
+        setValor(valor * 2) 
+    }
+    return(
+        <View style={estilo.view}>
+            <Text style={estilo.texto}>
+                {valor}
+            </Text>
+        <Pressable style={estilo.botao} onPress={() => Dobrar()}> 
+        <Text style={estilo.textoBotao}> Dobra e passa pro proximo vagabundo ❤ </Text>
+        </Pressable>
+    </View>)
+}
