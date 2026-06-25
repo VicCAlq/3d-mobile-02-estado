37e88c368602d9f6ccf51d02dc670a8881b84f42
thiacstaa <anthoniely.costa@nave.org.br>
2026-06-25 10:16:51 -0300

    atividade

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,73 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+
+import { View, Text, Pressable, StyleSheet, TextInput} from 'react-native'
+import { useState } from 'react'
+
+// Estilos de "CSS"
+const estilo = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+})
+
+export default function AtvCumprimenta() {
+ const [texto, setValorAtual] = useState('');
+
+ function novonome() {
+    setValorAtual()
+  }
+
+  function perderPonto() {
+    setPlacar(placar - 1)
+  }
+
+  return(
+    <View style={estilo.view}>
+      <Text style={estilo.texto}>
+        "Olá, qual seu nome?"
+      </Text>
+         <TextInput 
+        placeholder="Digite um nome"
+        onChangeText={(texto) => setValorAtual(texto)}
+      />
+       <Pressable style={estilo.botao} onPress={() => console.log("")}>
+        </Pressable>
+
+      <View style={estilo.viewBotoes}>
+        <Pressable style={estilo.botao} onPress={() => Dobrar()}>
+          <Text style={estilo.textoBotao}>dobrar</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,65 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <View> contendo um <Text>, que vai exibir este número recebido como argumento, 
 e um <Pressable>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import { useState } from 'react'
+
+// Estilos de "CSS"
+const estilo = StyleSheet.create({
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+})
+
+export default function AtvDobra() {
+  // Criação da variável "placar" e da função que altera seu valor
+  const [numero, setNumero] = useState(1)
+
+  // Funções que vão ser executadas pelos botões
+  function Dobrar() {
+    setNumero(numero * 2)
+  }
+
+  // Parte visual do componente
+  return(
+    <View style={estilo.view}>
+      <Text style={estilo.texto}>
+        {numero}
+      </Text>
+      <View style={estilo.viewBotoes}>
+        <Pressable style={estilo.botao} onPress={() => Dobrar()}>
+          <Text style={estilo.textoBotao}>dobrar</Text>
+        </Pressable>
+      </View>
+    </View>
+  )
+}
