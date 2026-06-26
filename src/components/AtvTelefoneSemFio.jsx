f1dd2df9c662e934def53d505320e3a8ef843dc5
Azulejo-barroco <enzo.almeida@nave.org.br>
2026-06-26 13:52:33 -0300

    faltando 1

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -7,3 +7,32 @@ Cada <Pressable> ao ser clicado, deve inserir na <View> abaixo um <Text> e um <T
 e substituir o conteúdo anterior dessa <View> de baixo dos botões.
 O texto digitado no <TextInput> ativado por um <Pressable> deve ser exibido no <Text> do outro <Pressable>, e vice-versa.
 */
+
+
+
+import { View, Text, Pressable, StyleSheet, TextInput } from 'react-native'
+import { useState } from 'react'
+
+export default function AtvTelefoneSemFio(){
+   
+    const [textoInserido, setTextoInserido] = useState("")
+    const [texto, setTexto] = useState("jkjkjkj")
+
+}
+
+
+return(
+    <View>
+        <Pressable>
+        onChangeText={setTextoInserido}
+        placeholder="Digite aqui o novo texto"
+        </Pressable>
+         <Pressable>
+         onChangeText={setTextoInserido}
+        placeholder="Digite aqui o novo texto"
+        </Pressable>
+        <View>
+        
+    </View>
+    </View>
+)
