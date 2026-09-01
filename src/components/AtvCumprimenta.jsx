9ed1c6017b6ca6fb816a46229b1ceeb1388b1981
loisa188 <mariaheloisa.silva@nave.org.br>
2026-09-01 10:23:08 -0300

    Update AtvCumprimenta.jsx

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,54 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+
+import React, { useState } from "react";
+import { View, Text, TextInput, Pressable } from "react-native";
+
+export default function AtvCumprimenta() {
+  const [nome, setNome] = useState("");
+  const [mensagem, setMensagem] = useState("");
+
+  function salvarNome() {
+    setMensagem(`É um prazer lhe conhecer, ${nome}`);
+  }
+
+  return (
+    <View style={{ padding: 20 }}>
+      <Text style={{ marginBottom: 10 }}>
+        EAE, qual é a sua graça?
+      </Text>
+
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <TextInput
+          style={{
+            borderWidth: "1px",
+            width: "200px",
+            padding: "8px",
+            marginRight: "10px",
+          }}
+          placeholder="Nome aqui"
+          value={nome}
+          onChangeText={setNome}
+        />
+
+        <Pressable
+          style={{
+            backgroundColor: "#7727c2",
+            padding: "10px",
+            borderRadius: "5px",
+          }}
+          onPress={salvarNome}
+        >
+          <Text>Salvar</Text>
+        </Pressable>
+      </View>
+
+      {mensagem !== "" && (
+        <Text style={{ marginTop: 20 }}>
+          {mensagem}
+        </Text>
+      )}
+    </View>
+  );
+}
