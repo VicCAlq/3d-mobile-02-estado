d349de0d8b990efbc0f0c13d9172da66a818791e
silvazx10 <kaua.correia@nave.org.br>
2026-09-01 11:22:36 -0300

    Update AtvMudaConteudo.jsx

--- src/components/AtvMudaConteudo.jsx
+++ src/components/AtvMudaConteudo.jsx
@@ -8,3 +8,35 @@ Ao clicar no segundo <Pressable>, o conteúdo dessa <View> deve ser substituído
 onde cada uma destas <Views> deve ter o tamanho de 200px de altura por 200px de largura, 
 a da esquerda deve ter a cor "purple", a do meio a cor "teal" e a da direita a cor "orange".
 */
+
+
+import React, { useState } from "react";
+import { View, Text, Pressable } from "react-native";
+
+export default function AtvMudaConteudo(){
+    const [pagina, setPagina] = useState("");
+
+return(
+  <View>{} 
+    <View style={{ flexDirection: "row", gap: 10 }}> 
+        <Pressable onPress={() => setPagina("texto")}>
+            <Text>Página 1</Text> 
+        </Pressable> 
+        <Pressable onPress={() => setPagina("cores")}>
+            <Text>Página 2</Text>
+        </Pressable>
+      </View>{}
+
+    <View style={{ marginTop: 20 }}>{
+    pagina === "texto" && ( 
+    <Text>Página em construção</Text> )}{
+    pagina === "cores" && ( 
+
+    <View style={{ flexDirection: "row" }}>
+        <View style={{ width: "200px", height: "200px", backgroundColor: "dark blue", }}/>
+        <View style={{ width: "200px", height: "200px", backgroundColor: "green", }}/>
+      <View style={{ width: "200px", height: "200px", backgroundColor: "yellow", }}/>
+
+      </View> )}
+      </View>
+    </View> ) };
