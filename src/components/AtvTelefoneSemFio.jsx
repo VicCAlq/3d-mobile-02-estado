96274cb7b15b5ff148d5db689f143ed07520b260
silvazx10 <kaua.correia@nave.org.br>
2026-09-01 11:22:59 -0300

    Update AtvTelefoneSemFio.jsx

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -7,3 +7,63 @@ Cada <Pressable> ao ser clicado, deve inserir na <View> abaixo um <Text> e um <T
 e substituir o conteúdo anterior dessa <View> de baixo dos botões.
 O texto digitado no <TextInput> ativado por um <Pressable> deve ser exibido no <Text> do outro <Pressable>, e vice-versa.
 */
+
+
+
+import { View, Text, Pressable, StyleSheet, TextInput } from 'react-native'
+import { useState } from 'react'
+
+
+export default function AtvTelefoneSemFio(){
+   
+
+    const [pagina, setPagina] = useState("");
+const [texto1, setTexto1] = useState("");
+const [texto2, setTexto2] = useState("");
+
+
+return(
+<View>
+    <View style={{ flexDirection: "row" }}>
+    <Pressable onPress={() => setPagina("1")}>
+        <Text>Botão 1</Text>
+    </Pressable>
+
+    <Pressable onPress={() => setPagina("2")}>
+        <Text>Botão 2</Text>
+    </Pressable>
+</View>
+
+<View style={{ marginTop: 20 }}>
+
+    {pagina === "1" && (
+        <View>
+            <Text>{texto2}</Text>
+
+            <TextInput
+                value={texto1}
+                onChangeText={setTexto1}
+                placeholder="insira o texto"
+            />
+        </View>
+    )}
+
+    {pagina === "2" && (
+        <View>
+            <Text>{texto1}</Text>
+
+            <TextInput
+                value={texto2}
+                onChangeText={setTexto2}
+                placeholder="insira o texto "
+            />
+        </View>
+    )}
+
+</View>
+      
+</View>
+
+    )
+    
+    }
